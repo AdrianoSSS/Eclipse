5f02ce4be5af88beade57ab42ddd4756b30a9855
AdrianoSSS <143419181+AdrianoSSS@users.noreply.github.com>
2024-04-18 11:10:53 -0300

    Programa.java

--- Programa.java
+++ Programa.java
@@ -1,12 +1,14 @@
 package aplicacao;
 import java.util.Scanner;
-import javax.swing.JOptionPane;
+//import javax.swing.JOptionPane;
+
+
 public class Programa {
 	public static void main(String[] args) {
 	Scanner input = new Scanner(System.in);
 	Produto produto = new Produto();
 	System.out.println("Informe o produto que será cadastrado: ");
-	produto.nome = input.nextLine();
+	produto.produto = input.nextLine();
 	
 	System.out.println("Informe a quantidade: ");
 	produto.quantidade = input.nextDouble();
@@ -14,19 +16,19 @@ public class Programa {
 	System.out.println("Informe o preço unitário: ");
 	produto.preco = input.nextFloat();
 	
-	System.out.println("Confirmação: " + produto.nome+", estoque atual: "+produto.quantidade+", preço: "+ produto.preco);
+	System.out.println("Confirmação: " + produto.produto+", estoque atual: "+produto.quantidade+", preço: "+ produto.preco);
 	
 	System.out.println("Deseja adicionar mais produtos? Quantos: ");
 	int estoque = input.nextInt();
 	produto.AddProduto(estoque);
 	
-	System.out.println("Atualização: " + produto.nome+", estoque atual: "+produto.quantidade+", preço: "+ produto.preco);
+	System.out.println("Atualização: " + produto.produto+", estoque atual: "+produto.quantidade+", preço: "+ produto.preco);
 	
 	System.out.println("Atualmente, alguma venda foi realizada? Quantas?");
 	estoque = input.nextInt(estoque);
 	produto.SubProduto(estoque);
-	System.out.println("Atualização pós vendas: " + produto.nome+", estoque atual: "+produto.quantidade+", preço: "+ produto.preco);
-	JOptionPane.showConfirmDialog(null,"Atualização pós vendas: " + produto.nome+", estoque atual: "+produto.quantidade+", preço: "+ produto.preco);
+	//JOptionPane.showConfirmDialog(null,"Atualização pós vendas: " + produto.produto+", estoque atual: "+produto.quantidade+", preço: "+ produto.preco);
+
 	input.close();
     
 	
