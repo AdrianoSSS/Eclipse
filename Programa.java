4aed0af42f485f1205855053d2aa4e1252a146d4
AdrianoSSS <143419181+AdrianoSSS@users.noreply.github.com>
2024-04-18 08:05:09 -0300

    Programa.java

--- Programa.java
+++ Programa.java
@@ -1,31 +1,39 @@
 package aplicacao;
 import java.util.Scanner;
-//import javax.swing.JOptionPane;//
+import javax.swing.*;
 
 public class Programa {
+
 	public static void main(String[] args) {
-		Scanner teclado = new Scanner(System.in);
-		Produto produto = new Produto();
-		System.out.println("Informe o produto que sera cadastrado : ");
-		produto.produto=teclado.next();
-		
-		System.out.println("Informe a quantidade : ");
-		produto.quantidade=teclado.nextDouble();
-		
-		System.out.println("Informe o preço individual : ");
-		produto.preco=teclado.nextDouble();
-		
-		System.out.println("confirmação: "+produto.produto+"\nEstoque atual : \n"+produto.quantidade+"\npreço : \n"+produto.preco);
-		System.out.println("\nDeseja inserir mais produtos ? quantos : \n");
-		int estoque=teclado.nextInt();
-		produto.AddProduto(estoque);
-		System.out.println("\nAtualização: "+produto.produto+"\nEstoque atual : \n"+produto.quantidade+"\npreço : \n"+produto.preco);
-		
-		System.out.println("\nAtualmente, alguma venda foi realizada ? \n");
-		estoque=teclado.nextInt();
-		produto.SubProduto(estoque);
-		System.out.println("\nAtualização pós vendas: \n"+produto.produto+"\nEstoque atual : \n"+produto.quantidade+"\npreço : \n"+produto.preco+"\nValor do estoque : \n"+produto.preco*produto.quantidade);
-	//JOptionPane.showConfirmDialog(null,"\nAtualização pós vendas: \n"+produto.produto+"\nEstoque atual : \n"+produto.quantidade+"\npreço : \n"+produto.preco+"\nValor do estoque : \n"+produto.preco*produto.quantidade);//
-	teclado.close();
+	Scanner input = new Scanner(System.in);
+	
+	Produtos produto = new Produtos();
+	
+	System.out.println("Informe o produto que será cadastrado: ");
+	produto.nome = input.nextLine();
+	
+	System.out.println("Informe a quantidade: ");
+	produto.quantidade = input.nextDouble();
+	
+	System.out.println("Informe o preço unitário: ");
+	produto.preco = input.nextFloat();
+	
+	System.out.println("Confirmação: " + produto.nome+", estoque atual: "+produto.quantidade+", preço: "+ produto.preco);
+	
+	System.out.println("Deseja adicionar mais produtos? Quantos: ");
+	int estoque = input.nextInt();
+	produto.AddProduto(estoque);
+	
+	System.out.println("Atualização: " + produto.nome+", estoque atual: "+produto.quantidade+", preço: "+ produto.preco);
+	
+	System.out.println("Atualmente, alguma venda foi realizada? Quantas?");
+	estoque = input.nextInt(estoque);
+	produto.SubProduto(estoque);
+	System.out.println("Atualização pós vendas: " + produto.nome+", estoque atual: "+produto.quantidade+", preço: "+ produto.preco);
+	
+	//JOptionPane.showConfirmDialog(null,"Atualização pós vendas: " + produto.nome+", estoque atual: "+produto.quantidade+", preço: "+ produto.preco);
+	input.close();
+    
+	
 	}
 }
